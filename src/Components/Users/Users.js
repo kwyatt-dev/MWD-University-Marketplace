bf2796965827ee629193451a03955a7fa47a172b
lchen9 <lchen9@nd.edu>
2023-03-05 17:35:58 -0500

    Parse models for Product Listings and Users

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,10 +1,12 @@
 import React from "react";
+import UsersList from "./UsersList";
 
+// This Component should only be visible to admins. Users may sign up/log in using another component in the future.
 const UsersModule = () => {
   return (
     <div>
       <h1> Feature-4 Kickoff Ancient Japan Dropouts </h1>
-      Users Page
+      <UsersList />
     </div>
   );
 };
--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -4,24 +4,23 @@ import Parse from "parse/dist/parse.min.js";
 // const url =
 //   "https://my-json-server.typicode.com/kellybuchanan/WebDev-Spring2021";
 
-// Get All Product Listings
-export async function getAllProductListings() {
-  const ProductListing = Parse.Object.extend("ProductListing");
-  const query = new Parse.Query(ProductListing);
-  // You can also query by using a parameter of an object
-  // query.equalTo('objectId', 'xKue915KBG');
-  try {
-    const results = await query.find();
-    console.log("Results: ", results);
-    return results;
-
-    //return listingsArray;
-  } catch (error) {
-    console.error("Error while fetching ProductListing", error);
-  }
+// Read
+export async function getAllUsers(){
+    const User = Parse.Object.extend("User");
+    const query = new Parse.Query(User);
+    // You can also query by using a parameter of an object
+    // query.equalTo('objectId', 'xKue915KBG');
+    try {
+      const results = await query.find();
+      console.log("Results: ", results);
+      return results;
+      //return listingsArray;
+    } catch (error) {
+      console.error("Error while fetching User", error);
+    }
 }
 
-// Delete
+// Delete: not currently in use due to auth restrictions
 export async function deleteProductListing(objectId) {
   const query = new Parse.Query('ProductListing');
   try {
