60c7d23feee277fa91d92bc1f5b245b05a867eaf
lchen9 <lchen9@nd.edu>
2023-04-27 22:08:23 -0400

    Now creating profile on register

--- src/Components/Auth/PasswordReset.js
+++ src/Components/Auth/PasswordReset.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { resetPassword } from "./AuthService";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 export default function PasswordReset(){
 
     const {email} = useParams();
     return(
         <div>
             <button type="submit" className="btn btn-primary" onSubmit={resetPassword(email)}>Reset Your Password</button>
+            <Link to="/auth/login">Go back</Link>
         </div>
     )
 };
